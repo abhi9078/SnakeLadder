df59e94ae8ecfcf7cb680e5fd0362fd423b8f779
Abhilash <abhilashmeher1234@gmail.com>
2022-03-13 16:07:28 +0530

    Added Program File

--- Program.cs
+++ Program.cs
@@ -17,10 +17,44 @@ namespace SnakeLadder
             int num=rnd.Next(1,7);
             Console.WriteLine("The Random Dice Number is: " + num);
         }
+
+        public static void UC_3()
+        {
+            int ladder = 1, ldice;
+            int snake = 2, sdice;
+            Console.WriteLine("Welcome to UC_3");
+            Random random = new Random();
+            int randomCheck=random.Next(3);
+
+            switch (randomCheck)
+            {
+                case 1:
+                    {
+                        Random rnd = new Random();
+                        ldice = rnd.Next(1,7);
+                        Console.WriteLine("Got Ladder");
+                        break;
+                    }
+                case 2:
+                    {
+                        Random sran = new Random();
+                        sdice = sran.Next(1,7);
+                        Console.WriteLine("Got snake");
+                        break ;
+                    }
+                default:
+                    {
+                        Console.WriteLine("No play, Stay at base");
+                        break;
+                    }
+            }
+
+        }
         static void Main(string[] args)
         {
             Program.UC_1();
             Program.UC_2();
+            Program.UC_3();
         }
     }
 }
