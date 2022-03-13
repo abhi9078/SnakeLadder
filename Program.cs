1f2d1567b5a4e83883a4a3b6b601bc0a9aca4c9e
Abhilash <abhilashmeher1234@gmail.com>
2022-03-13 16:22:43 +0530

    Added UC_4 Program Files

--- Program.cs
+++ Program.cs
@@ -50,11 +50,60 @@ namespace SnakeLadder
             }
 
         }
+
+        public static void UC_4()
+        {
+            int ladder = 1, ldice;
+            int snake = 2, sdice;
+            int win = 100;
+            int count = 0;
+            Console.WriteLine("Welcome to UC_4");
+            while (count <= win)
+            {
+                if (count >= 0)
+                {
+                    Random random = new Random();
+                    int randomCheck = random.Next(3);
+
+                    switch (randomCheck)
+                    {
+                        case 1:
+                            {
+                                Random rnd = new Random();
+                                ldice = rnd.Next(1, 7);
+                                count = count + ldice;
+                                Console.WriteLine("Got Ladder");
+                                break;
+                            }
+                        case 2:
+                            {
+                                Random sran = new Random();
+                                sdice = sran.Next(1, 7);
+                                count = count - sdice;
+                                Console.WriteLine("Got snake");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("No play");
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+                
+            }
+            Console.WriteLine("Player wins" + count);
+        }
         static void Main(string[] args)
         {
             Program.UC_1();
             Program.UC_2();
             Program.UC_3();
+            Program.UC_4();
         }
     }
 }
