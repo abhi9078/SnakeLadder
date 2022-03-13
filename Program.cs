41eb9e8864f89f348dfa9faee071f7fb14248410
Abhilash <abhilashmeher1234@gmail.com>
2022-03-13 15:55:29 +0530

    Added UC_2 program files

--- Program.cs
+++ Program.cs
@@ -9,9 +9,18 @@ namespace SnakeLadder
             Console.WriteLine("Welcome to Snake and Ladder Problem");
             Console.WriteLine("Snake and ladder game played with singel player start at zero");
         }
+
+        public static void UC_2()
+        {
+            Console.WriteLine("Welcome to UC-2 program");
+            Random rnd = new Random();
+            int num=rnd.Next(1,7);
+            Console.WriteLine("The Random Dice Number is: " + num);
+        }
         static void Main(string[] args)
         {
             Program.UC_1();
+            Program.UC_2();
         }
     }
 }
