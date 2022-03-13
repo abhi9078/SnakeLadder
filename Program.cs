dbdf152a48c9a8153239796b45f1ee78eaed2194
Abhilash <abhilashmeher1234@gmail.com>
2022-03-13 15:51:23 +0530

    Added UC-1 Program Files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        public static void UC_1()
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Problem");
+            Console.WriteLine("Snake and ladder game played with singel player start at zero");
+        }
+        static void Main(string[] args)
+        {
+            Program.UC_1();
+        }
+    }
+}
