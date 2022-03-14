e859a0069ed6ff725cdda331e84023aa1a8445e1
Abhilash <abhilashmeher1234@gmail.com>
2022-03-14 09:56:32 +0530

    Added UC_5 program files

--- Program.cs
+++ Program.cs
@@ -98,12 +98,66 @@ namespace SnakeLadder
             }
             Console.WriteLine("Player wins" + count);
         }
+
+        public static void UC_5()
+        {
+            int ladder = 1, ldice=0;
+            int snake = 2, sdice;
+            int win = 100;
+            int count = 0,a=0;
+            Console.WriteLine("Welcome to UC_5");
+            while (count != win)
+            {
+                if (count >= 0 && count<100)
+                {
+                    Random random = new Random();
+                    int randomCheck = random.Next(3);
+
+                    switch (randomCheck)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Got Ladder");
+                                Random rnd = new Random();
+                                ldice = rnd.Next(1, 7);
+                                count = count + ldice;
+                                a = count;
+                                break;
+                            }
+                        case 2:
+                            {
+                                Random sran = new Random();
+                                sdice = sran.Next(1, 7);
+                                count = count - sdice;
+                                Console.WriteLine("Got snake");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("No play");
+                                break;
+                            }
+                    }
+                }
+                else if (a > 100)
+                {
+                    count = a - ldice;
+                }
+                else
+                {
+                    count = 0;
+                }
+
+            }
+            Console.WriteLine("Player wins" + count);
+        }
         static void Main(string[] args)
         {
             Program.UC_1();
             Program.UC_2();
             Program.UC_3();
             Program.UC_4();
+            Program.UC_5();
         }
     }
 }
